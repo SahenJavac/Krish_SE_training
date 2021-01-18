7bf21acdde8a2fe3e21272b594f0150442974a64
SahenJavac <54188335+SahenJavac@users.noreply.github.com>
2021-01-18 16:22:15 +0530

    Add files via upload

---
+++ Exception/StackTrace/geniusmobile/exceptions/InvalidBrandException.java
@@ -0,0 +1,13 @@
+package geniusmobile.exceptions;
+
+/**
+ *
+ * @author sahen
+ */
+public class InvalidBrandException extends Exception {
+
+    public InvalidBrandException(String message, Exception e) {
+        super(message, e);
+    }
+
+}
---
+++ Exception/StackTrace/geniusmobile/exceptions/InvalidModelNoException.java
@@ -0,0 +1,13 @@
+package geniusmobile.exceptions;
+
+/**
+ *
+ * @author sahen
+ */
+public class InvalidModelNoException extends Exception {
+
+    public InvalidModelNoException(String message, Exception e) {
+        super(message, e);
+    }
+
+}
