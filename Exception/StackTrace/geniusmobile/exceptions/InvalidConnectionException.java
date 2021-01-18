03b063a076611ebb0a978bc02b2e3b84af2eb593
SahenJavac <54188335+SahenJavac@users.noreply.github.com>
2021-01-18 22:44:51 +0530

    Add files via upload

---
+++ Exception/StackTrace/geniusmobile/exceptions/InvalidConnectionException.java
@@ -0,0 +1,13 @@
+package geniusmobile.exceptions;
+
+/**
+ *
+ * @author sahen
+ */
+public class InvalidConnectionException extends RuntimeException {
+
+    public InvalidConnectionException(String message) {
+        super(message);
+    }
+
+}
---
+++ Exception/StackTrace/geniusmobile/exceptions/OutOfQuantityException.java
@@ -0,0 +1,13 @@
+package geniusmobile.exceptions;
+
+/**
+ *
+ * @author sahen
+ */
+public class OutOfQuantityException extends RuntimeException {
+
+    public OutOfQuantityException(String message) {
+        super(message);
+    }
+
+}
